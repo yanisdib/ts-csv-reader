9aa3b29af68b81ea86a31386214e63768c7e4e83
Yanis Dib <ynsprive@gmail.com>
2022-03-07 00:33:58 +0100

    Refactor code

---
+++ src/CsvFileReader.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+
+export class CsvFileReader {
+    data: string[][] = [];
+
+    constructor(public filename: string) { }
+
+    read(): void {
+        this.data = fs.readFileSync('football.csv', { encoding: 'utf-8' })
+            .split('\n')
+            .map((match: string): string[] => match.split(','))
+    }
+}
