c60261c7b881878d386d3f83dca554b0ca3e57ed
Yanis Dib <ynsprive@gmail.com>
2022-03-07 00:39:24 +0100

    Add dateStringToDate parser

---
+++ src/utils/parser.ts
@@ -0,0 +1,6 @@
+export const dateStringToDate = (dateString: string): Date => {
+    const dateParts = dateString.split('/')
+        .map((datePart: string): number => parseInt(datePart));
+
+    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0])
+}
